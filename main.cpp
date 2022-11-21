1c9b2fb614185544c8f03e0990016b380f7948df
Pedro Guimarães <105455410+pedrin0x10@users.noreply.github.com>
2022-11-20 23:51:59 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <windows.h>
+#include <iostream>
+
+typedef int(__fastcall* rPRINT)(std::string const&, std::string const&);rPRINT PRINT = reinterpret_cast<rPRINT>(GetProcAddress(GetModuleHandleA("conhost-v2.dll"), "?Print@ConHost@@YAXAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@0@Z"));
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) 
+{
+	//Leave "script:" in the begin of first string to work fine
+	PRINT("script:PEDRIN", "Fivem is gay\n");
+}
